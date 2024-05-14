59b4bc279513218fb3e0683b445e886eb1457036
Pop Sergiu <sergiugeorgepop@gmail.com>
2024-05-14 20:59:07 +0300

    Finished skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <button>Generate Cat Fact</button>
-      <p></p>
-    </div>
-  );
+const URL = 'https://catfact.ninja/fact'
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        catFact: '',
+        length: ''
+      }
+    }
+
+  getFact = () => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        const {fact, length} = data;
+        this.setState({
+          catFact: fact,
+          length: length
+        })
+      })
+      .catch((error) => {
+        console.error('Error fetching cat fact:', error)
+      })
+  }
+
+  render(){
+    return(
+      <div>
+        <button onClick={this.getFact}>Get Fact!</button>
+        <p>{this.state.catFact}</p>
+        <p>It has this length: {this.state.length}</p>
+      </div>
+    )
+  }
 }
 
-export default App;
+
